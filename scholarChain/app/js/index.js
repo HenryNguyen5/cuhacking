5bfe8fdc21bd0887ee473bf134539b51b7650035
NickLewanowicz <nicklewanowicz@gmail.com>
2017-03-04 16:04:51 -0500

    js tweaks

--- scholarChain/app/js/index.js
+++ scholarChain/app/js/index.js
@@ -5,36 +5,23 @@ var addToLog = function(id, txt) {
 };
 
 // ===========================
-// Blockchain example
+// Register User
 // ===========================
-$(document).ready(function() {
 
-  $("#blockchain button.set").click(function() {
-    var value = parseInt($("#blockchain input.text").val(), 10);
-    SimpleStorage.set(value);
-    addToLog("#blockchain", "SimpleStorage.set(" + value + ")");
-  });
-
-  $("#blockchain button.get").click(function() {
-    SimpleStorage.get().then(function(value) {
-      $("#blockchain .value").html(value.toNumber());
-    });
-    addToLog("#blockchain", "SimpleStorage.get()");
-  });
-
-});
-
-// ===========================
-// Storage (IPFS) example
-// ===========================
 $(document).ready(function() {
   EmbarkJS.Storage.setProvider('ipfs',{server: 'localhost', port: '5001'});
 
-  $("#storage button.setIpfsText").click(function() {
-    var value = $("#storage input.ipfsText").val();
-    EmbarkJS.Storage.saveText(value).then(function(hash) {
-      $("span.textHash").html(hash);
-      $("input.textHash").val(hash);
+  $("#register button.submit").click(function() {
+    var value = $("#register input.bank").val();
+    let s =  {
+      "name": "Entrance Scholarship",
+      "description": "Easy Money",
+      "value": 1000,
+      "releaseDate": "09/09/2017",
+      "deadline": "09/01/2017"
+    }
+    EmbarkJS.Storage.saveText(JSON.stringify(s)).then((hash) => {
+      console.log("scholarship was saved with hash" + hash);
     });
     addToLog("#storage", "EmbarkJS.Storage.saveText('" + value + "').then(function(hash) { })");
   });
@@ -47,55 +34,4 @@ $(document).ready(function() {
     addToLog("#storage", "EmbarkJS.Storage.get('" + value + "').then(function(content) { })");
   });
 
-  $("#storage button.uploadFile").click(function() {
-    var input = $("#storage input[type=file]");
-    EmbarkJS.Storage.uploadFile(input).then(function(hash) {
-      $("span.fileIpfsHash").html(hash);
-      $("input.fileIpfsHash").val(hash);
-    });
-    addToLog("#storage", "EmbarkJS.Storage.uploadFile($('input[type=file]')).then(function(hash) { })");
-  });
-
-  $("#storage button.loadIpfsFile").click(function() {
-    var hash = $("#storage input.fileIpfsHash").val();
-    var url = EmbarkJS.Storage.getUrl(hash);
-    var link = '<a href="' + url + '" target="_blank">' + url + '</a>';
-    $("span.ipfsFileUrl").html(link);
-    $(".ipfsImage").attr('src', url);
-    addToLog("#storage", "EmbarkJS.Storage.getUrl('" + hash + "')");
-  });
-
 });
-
-// ===========================
-// Communication (Whisper) example
-// ===========================
-$(document).ready(function() {
-
-  $("#communication .error").hide();
-  web3.version.getWhisper(function(err, res) {
-    if (err) {
-      $("#communication .error").show();
-    } else {
-      EmbarkJS.Messages.setProvider('whisper');
-    }
-  });
-
-  $("#communication button.listenToChannel").click(function() {
-    var channel = $("#communication .listen input.channel").val();
-    $("#communication #subscribeList").append("<br> subscribed to " + channel + " now try sending a message");
-    EmbarkJS.Messages.listenTo({topic: [channel]}).then(function(message) {
-      $("#communication #messagesList").append("<br> channel: " + channel + " message: " + message);
-    });
-    addToLog("#communication", "EmbarkJS.Messages.listenTo({topic: ['" + channel + "']}).then(function(message) {})");
-  });
-
-  $("#communication button.sendMessage").click(function() {
-    var channel = $("#communication .send input.channel").val();
-    var message = $("#communication .send input.message").val();
-    EmbarkJS.Messages.sendMessage({topic: channel, data: message});
-    addToLog("#communication", "EmbarkJS.Messages.sendMessage({topic: '" + channel + "', data: '" + message + "'})");
-  });
-
-});
-
