c76580c1d3f7363337a771d32381447c886c0da2
NickLewanowicz <nicklewanowicz@gmail.com>
2017-03-04 21:04:27 -0500

    ui

--- scholarChain/app/js/index.js
+++ scholarChain/app/js/index.js
@@ -10,31 +10,45 @@ var addToLog = function(id, txt) {
 
 $(document).ready(function() {
   EmbarkJS.Storage.setProvider('ipfs',{server: 'localhost', port: '5001'});
-
+  var scholarships = ['United States', 'Canada', 'Argentina', 'Armenia'];
+  var sList = $('ul.list-group')
+  $.each(scholarships, function(i){
+    var li = $('<li/>')
+        .addClass('list-group-item')
+        .appendTo(sList);
+    var aaa = $('<a/>')
+        .addClass('blah')
+        .attr('href', '../scholarship/' + scholarships[i])
+        .text(scholarships[i] + ' - $' + 'scholarships[i].value')
+        .appendTo(li);
+  });
   $("#register button.submit").click(function() {
-    //var value = $("#register input.bank").val();
-    // let s =  {
-    //   "name": "Entrance Scholarship",
-    //   "description": "Easy Money",
-    //   "value": 1000,
-    //   "releaseDate": "09/09/2017",
-    //   "deadline": "09/01/2017"
-    // }
     let name = $("#register input.register-name").val();
     let wallet = $("#register input.register-id").val();
     let user = {name, wallet}
 
     EmbarkJS.Storage.saveText(JSON.stringify(user)).then((hash) => {
-       console.log(JSON.stringify("User '" + name +  "' was saved with hash " hash));
-       scholarChain.registerPerson(wallet, hash).then((result) => {
-         console.log('Successful:'+ result);
+       console.log(JSON.stringify("User '" + name +  "' was saved with hash " + hash));
+       ScholarChain.registerPerson(wallet, hash).then((result) => {
+         console.log('Successful:'+ [Boolean(result),String(result),Number(result),Boolean(result)]);
        })
     });
-    // EmbarkJS.Storage.get('QmYdC6dNLedu6m8Eri4HiVmQWvPmjeamfW4WLMAAJZGfPo').then((content) => {
-    //   console.log(content.slice(4, -2));
-    //   console.log(JSON.parse(content.slice(4, -2)).name)
-    // });
-    //addToLog("#register", "EmbarkJS.Storage.saveText('" + value + "').then(function(hash) { })");
+  });
+
+  $("#create button.submit").click(function() {
+    debugger
+    let title = $("#create input.scholar-title").val();
+    let description = $("#create input.scholar-description").val();
+    let value = $("#create input.scholar-value").val();
+    let release = $("#create input.scholar-release").val();
+    let deadline = $("#create input.scholar-deadline").val();
+    let scholarship = {title, description, value, release, deadline}
+    EmbarkJS.Storage.saveText(JSON.stringify(scholarship)).then((hash) => {
+       console.log(JSON.stringify("Scholarship '" + title +  "' was saved with hash " + hash));
+      //  ScholarChain.registerPerson(wallet, hash).then((result) => {
+      //    console.log('Successful:'+ [Boolean(result),String(result),Number(result),Boolean(result)]);
+      //  })
+    });
   });
 
 });
